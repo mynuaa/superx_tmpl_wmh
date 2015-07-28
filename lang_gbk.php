7dbef0efdf1384e12051c64f52c52c68569eb09f
Rex Zeng <rex.fgtsky@qq.com>
2015-07-28 15:10:59 +0800

    .

---
+++ lang_gbk.php
@@ -0,0 +1,18 @@
+<?php
+
+$SC_LANG = array(
+    'newsrec' => '新闻推荐',
+    'more' => '更多',
+    'entertainment' => '娱乐',
+    'domestic' => '国内',
+    'hotimg' => '热图',
+    'hotconcern' => '热门关注',
+    'rectheme' => '推荐主题',
+    'navigation' => '社区导航',
+    'getinto' => '进入社区',
+    'my' => '我的',
+    'share' => '分享',
+    'portal' => '门户',
+    'bbs' => '论坛',
+    'choice' => '请选择发帖版块',
+);
